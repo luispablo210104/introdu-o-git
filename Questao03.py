e14d0fc6d983a796e3c0926abdd3e5ffaa036ce8
luispablo210104 <166664747+luispablo210104@users.noreply.github.com>
2024-04-17 23:05:24 +0000

    QUESTÕES RESOLVIDAS!

---
+++ Questao03.py
@@ -0,0 +1,7 @@
+# Crie uma lista que tenha 10 valores e some todos eles.
+lista = [1,2,3,4,5,6,7,8,9,10]
+soma = 0
+for valor in lista:
+    soma += valor
+    print(soma)
+
---
+++ Questao04.py
@@ -0,0 +1,6 @@
+# Crie uma função em python que receba um número diga se o número é par ou ímpar.
+def par_impar(num):
+    if num % 2 == 0:
+        print(f'{num} é par')
+    else:
+        print(f'{num} é ímpar')
